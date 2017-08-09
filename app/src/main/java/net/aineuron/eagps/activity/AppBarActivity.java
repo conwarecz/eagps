1e6b1f78964c2dc2c15c385e8238a66195e2433d
Vit Veres <veres.vit@gmail.com>
2017-08-09 21:35:34 +0200

    Implemented local notifications

--- app/src/main/java/net/aineuron/eagps/activity/AppBarActivity.java
+++ app/src/main/java/net/aineuron/eagps/activity/AppBarActivity.java
@@ -1,13 +1,21 @@
 package net.aineuron.eagps.activity;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import net.aineuron.eagps.Appl;
 import net.aineuron.eagps.R;
 import net.aineuron.eagps.model.UserManager;
 import net.aineuron.eagps.model.database.User;
@@ -15,6 +23,7 @@ import net.aineuron.eagps.model.database.User;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenuItem;
+import org.androidannotations.annotations.SystemService;
 import org.androidannotations.annotations.res.ColorRes;
 
 /**
@@ -24,6 +33,9 @@ import org.androidannotations.annotations.res.ColorRes;
 
 @EActivity
 public class AppBarActivity extends MainActivityBase {
+
+	@SystemService
+	NotificationManager notificationManager;
 
 	@ColorRes(R.color.colorPrimary)
 	int primary;
@@ -42,6 +54,8 @@ public class AppBarActivity extends MainActivityBase {
 	private TextView profileName;
 	private ImageView stateIcon;
 	private TextView licencePlate;
+
+	private int currentNotificationID = 0;
 
 	@Override
 	public boolean onCreateOptionsMenu(final Menu menu) {
@@ -64,7 +78,30 @@ public class AppBarActivity extends MainActivityBase {
 
 	@OptionsItem(R.id.actin_offer)
 	void actionOffer() {
-		OfferActivity_.intent(this).start();
+
+		Bitmap icon = BitmapFactory.decodeResource(this.getResources(),
+				R.mipmap.ic_launcher);
+		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, Appl.NOTIFFICATIONS_CHANNEL_NAME)
+				.setSmallIcon(R.mipmap.ic_launcher)
+				.setLargeIcon(icon)
+				.setContentTitle("Nová zakázka")
+				.setContentText("Máte nabídku nové zakázky. Zobrazit detail.");
+
+		Intent notificationIntent = new Intent(this, OfferActivity_.class);
+
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+		notificationBuilder.setContentIntent(contentIntent);
+
+		Notification notification = notificationBuilder.build();
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+		notification.defaults |= Notification.DEFAULT_SOUND;
+		currentNotificationID++;
+		int notificationId = currentNotificationID;
+		if (notificationId == Integer.MAX_VALUE - 1)
+			notificationId = 0;
+
+		notificationManager.notify(notificationId, notification);
+
 	}
 
 	private void setUpActionBar() {
